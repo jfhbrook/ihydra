3e414ae1ffc659a13ca535e724417bd520dd7454
Josh Holbrook <josh.holbrook@gmail.com>
2020-02-07 11:01:46 -0500

    Dev mode works in OSX now

--- src/services/installer.js
+++ src/services/installer.js
@@ -7,7 +7,8 @@ const { exec } = require("../lib/process");
 
 function quote(xs) {
   if (process.platform !== "win32") {
-    return shellQuote(xs);
+    // shell-quote quotes the curlies, which actually breaks the templating
+    return shellQuote(xs).replace('\\{', '{').replace('\\}', '}');
   }
 
   // I cheese this really hard here, doing the bare minimum to make
