a8906b7931d0a93e1c67570ecbaa95520b09edc0
Josh Holbrook <josh.holbrook@gmail.com>
2020-02-07 11:36:00 -0500

    Moved hydra canvas to component

---
+++ src/renderer/components/Hydra/index.js
@@ -0,0 +1,29 @@
+const Hydra = require('hydra-synth')
+const React = require("react");
+const { useEffect, useRef } = React;
+
+const hydraCss = require("./index.css");
+
+const Server = require("../../../lib/kernel");
+
+// TODO: This should have loading screen logic kinda like the launcher
+module.exports = ({ config }) => {
+  const canvasRef = useRef();
+  const hydraRef = useRef();
+  const serverRef = useRef();
+
+  useEffect(() => {
+    config.logger.debug('Setting up Hydra...');
+    const canvas = canvasRef.current;
+
+    const hydra = new Hydra({ canvas });
+    hydraRef.current = hydra;
+
+    const server = new Server(config);
+    serverRef.current = server;
+
+    server.listen();
+  });
+
+  return <canvas ref={canvasRef} />;
+};
--- src/renderer/containers/kernel.js
+++ src/renderer/containers/kernel.js
@@ -1,29 +1,7 @@
-const Hydra = require('hydra-synth')
 const React = require("react");
-const { useEffect, useRef } = React;
-
-const hydraCss = require("./hydra.css");
-
-const Server = require("../../lib/kernel");
+const Hydra = require("../components/Hydra");
 
 // TODO: This should have loading screen logic kinda like the launcher
 module.exports = ({ config }) => {
-  const canvasRef = useRef();
-  const hydraRef = useRef();
-  const serverRef = useRef();
-
-  useEffect(() => {
-    config.logger.debug('Setting up Hydra...');
-    const canvas = canvasRef.current;
-
-    const hydra = new Hydra({ canvas });
-    hydraRef.current = hydra;
-
-    const server = new Server(config, canvasRef.current);
-    serverRef.current = server;
-
-    server.listen();
-  });
-
-  return <canvas ref={canvasRef} />;
+  return <Hydra config={config} />;
 };
