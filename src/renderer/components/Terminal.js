376bc73706a97203ec9da3a123695df5a6d393a2
Joshua Holbrook <josh.holbrook@gmail.com>
2020-02-08 15:29:43 -0500

    Stub terminal component for launching jupyter

---
+++ src/renderer/components/Terminal.js
@@ -0,0 +1,34 @@
+const React = require("react");
+
+const { useEffect, useRef } = React;
+const XTerm = require("xterm").Terminal;
+
+require("xterm/css/xterm.css");
+
+module.exports = function Terminal({ stdout, stderr }) {
+  const divRef = useRef();
+  const termRef = useRef();
+
+  useEffect(() => {
+    const div = divRef.current;
+    const term = new XTerm();
+
+    termRef.current = term;
+
+    term.open(div);
+
+    function onData(buf) {
+      term.write(buf.toString());
+    }
+
+    stdout.on("data", onData);
+    stderr.on("data", onData);
+
+    return () => {
+      stdout.removeListener("data", onData);
+      stderr.removeListener("data", onData);
+    };
+  });
+
+  return <div ref={divRef} />;
+};
