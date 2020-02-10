fcc9d116f3b24311cfe61c96c30b513395812ae1
Joshua Holbrook <josh.holbrook@gmail.com>
2020-02-10 17:59:59 -0500

    Tabbed interface

--- src/renderer/containers/launcher.js
+++ src/renderer/containers/launcher.js
@@ -22,10 +22,12 @@ const { cloneConfig } = require("../../lib/config");
 function useLauncherState(config) {
   const [state, rawSetState] = useState({
     status: "loading",
+    tab: "launcher",
     config
   });
   const { status } = state;
   const cfg = state.config;
+  const { tab } = state;
 
   function setState(newState) {
     if (newState.status !== status) {
@@ -44,7 +46,7 @@ function useLauncherState(config) {
     const config = cloneConfig(cfg);
     config.error = err;
     config.logger.error(err);
-    setState({ status, config });
+    setState({ status, config, tab });
   }
 
   function setStatusOnError(status) {
@@ -61,20 +63,20 @@ function useLauncherState(config) {
       "Configured Jupyter version appears invalid; Attempting an automatic search"
     );
     config = cfg.setJupyterCommand(null);
-    setState({ status: "searching", config });
+    setState({ status: "searching", config, tab });
   });
 
   function init() {
     const config = cfg.loadVersionInfo();
     if (config.jupyterCommand) {
-      return setState({ status: "registering", config });
+      return setState({ status: "registering", config, tab });
     }
-    return setState({ status: "searching", config });
+    return setState({ status: "searching", config, tab });
   }
 
   const searchForJupyter = confusedIfError(async () => {
     const config = await cfg.searchForJupyter();
-    setState({ status: "registering", config });
+    setState({ status: "registering", config, tab });
   });
 
   // TODO: Interstitial error state that displays the error
@@ -82,12 +84,12 @@ function useLauncherState(config) {
   const loadJupyterInfo = retrySearchIfError(async () => {
     const config = await cfg.loadJupyterInfo();
     config.ensureSupportedJupyterVersion();
-    setState({ status: "ready", config });
+    setState({ status: "ready", config, tab });
   });
 
   function useJupyterCommand(command) {
     const config = cfg.setJupyterCommand(command);
-    setState({ status: "registering", config });
+    setState({ status: "registering", config, tab });
   }
 
   const install = failedIfError(async () => {
@@ -97,7 +99,7 @@ function useLauncherState(config) {
 
   const runJupyter = confusedIfError(async () => {
     const jupyterProcess = await spawnJupyter(cfg);
-    setState({ status: "running", config: cfg, jupyterProcess });
+    setState({ status: "running", config: cfg, tab, jupyterProcess });
   });
 
   // TODO: Do this *gracefully*!
@@ -105,7 +107,7 @@ function useLauncherState(config) {
     const jupyter = state.jupyterProcess;
     jupyter.kill();
     jupyter.on("exit", () => {
-      setState({ status: "ready", config: cfg });
+      setState({ status: "ready", config: cfg, tab });
     });
   }
 
@@ -126,6 +128,10 @@ function useLauncherState(config) {
       runJupyter();
     default:
       break;
+  }
+
+  function setTab(tab) {
+    setState({ ...state, tab });
   }
 
   return {
@@ -150,6 +156,12 @@ function useLauncherState(config) {
       setStatus("launching");
     },
     stopJupyter,
+    helpTab() {
+      setTab("help");
+    },
+    launcherTab() {
+      setTab("launcher");
+    },
     exit() {
       ipcRenderer.send("bail");
     }
@@ -167,71 +179,89 @@ function Launcher({ config }) {
     tryInstall,
     launchJupyter,
     stopJupyter,
+    helpTab,
+    launcherTab,
     exit
   } = useLauncherState(config);
 
-  switch (state.status) {
-    case "loading":
-      return <LoadingScreen message="loading..." />;
-    case "searching":
-      return <LoadingScreen message="searching..." />;
-    case "which":
-      return (
-        <JupyterCommandFinder
-          config={state.config}
-          trySearching={trySearching}
-          useJupyterCommand={useJupyterCommand}
-          exit={exit}
-        />
-      );
-    case "registering":
-      return <LoadingScreen message="registering..." />;
-    case "ready":
-      return (
-        <div>
-          <h1>ready</h1>
-          <InstallerConfig config={state.config} />
-          <Button onClick={tryInstall}>install</Button>
-          <Button onClick={goBackToWhich}>
-            set command for starting jupyter
-          </Button>
-          <Button onClick={launchJupyter}>LAUNCH JUPYTER</Button>
-          <Button onClick={exit}>exit</Button>
-        </div>
-      );
-    case "installing":
-      return <LoadingScreen message="installing..." />;
-    case "launching":
-      return <LoadingScreen message="launching..." />;
-    case "running":
-      return (
-        <div>
-          <Terminal process={state.jupyterProcess} />
-          <Button onClick={stopJupyter}>Stop Jupyter</Button>
-        </div>
-      );
-    case "install_failed":
-      return (
-        <div>
-          <h1>install failed!</h1>
-          <Button onClick={goBackToMain}>ugh crap</Button>
-        </div>
-      );
-    case "install_succeeded":
-      return (
-        <div>
-          <h1>install succeeded!</h1>
-          <Button onClick={goBackToMain}>cool beans!</Button>
-        </div>
-      );
-    default:
-      return <h1>confused!</h1>;
+  function launcherUI() {
+    switch (state.status) {
+      case "loading":
+        return <LoadingScreen message="loading..." />;
+      case "searching":
+        return <LoadingScreen message="searching..." />;
+      case "which":
+        return (
+          <JupyterCommandFinder
+            config={state.config}
+            trySearching={trySearching}
+            useJupyterCommand={useJupyterCommand}
+            exit={exit}
+          />
+        );
+      case "registering":
+        return <LoadingScreen message="registering..." />;
+      case "ready":
+        return (
+          <div>
+            <h1>ready</h1>
+            <InstallerConfig config={state.config} />
+            <Button onClick={tryInstall}>install</Button>
+            <Button onClick={goBackToWhich}>
+              set command for starting jupyter
+            </Button>
+            <Button onClick={launchJupyter}>LAUNCH JUPYTER</Button>
+            <Button onClick={exit}>exit</Button>
+          </div>
+        );
+      case "installing":
+        return <LoadingScreen message="installing..." />;
+      case "launching":
+        return <LoadingScreen message="launching..." />;
+      case "running":
+        return (
+          <div>
+            <Terminal process={state.jupyterProcess} />
+            <Button onClick={stopJupyter}>Stop Jupyter</Button>
+          </div>
+        );
+      case "install_failed":
+        return (
+          <div>
+            <h1>install failed!</h1>
+            <Button onClick={goBackToMain}>ugh crap</Button>
+          </div>
+        );
+      case "install_succeeded":
+        return (
+          <div>
+            <h1>install succeeded!</h1>
+            <Button onClick={goBackToMain}>cool beans!</Button>
+          </div>
+        );
+      default:
+        return <h1>confused!</h1>;
+    }
   }
+
+  function helpUI() {
+    return <h1>help text here</h1>;
+  }
+
+  const TABS = { launcher: launcherUI, help: helpUI };
+
+  return (
+    <div>
+      <Button onClick={launcherTab}>Launcher</Button>
+      <Button onClick={helpTab}>About</Button>
+      {TABS[state.tab]()}
+    </div>
+  );
 }
 
 Launcher.propTypes = {
   config: PropTypes.shape({
-    action: PropTypes.oneOf(['launcher']).isRequired,
+    action: PropTypes.oneOf(["launcher"]).isRequired,
     logger: PropTypes.object.isRequired
   }).isRequired
 };
