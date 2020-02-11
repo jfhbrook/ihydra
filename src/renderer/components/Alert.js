13f39036e3d5acb28362a8707965fde57601e7d0
Joshua Holbrook <josh.holbrook@gmail.com>
2020-02-11 16:48:48 -0500

    Broken attempt at using styled-components button

--- src/renderer/components/Alert.js
+++ src/renderer/components/Alert.js
@@ -1,7 +1,7 @@
 import PropTypes from "prop-types";
 import React from "react";
 
-import Button from "./WizardButton";
+import Button from "./Button";
 
 export default function Alert({ hed, dek, buttons }) {
   return (
---
+++ src/renderer/components/Button.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+const Button = styled.button`
+  color: #f0f;
+  background-color: #fff;
+`;
+
+export default Button;
