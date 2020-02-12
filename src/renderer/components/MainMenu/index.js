3d5685d70ca1cb16427d74303db4ee3dbfc6522d
Joshua Holbrook <josh.holbrook@gmail.com>
2020-02-12 12:28:49 -0500

    Add styling to MainMenu

--- src/renderer/components/MainMenu/index.js
+++ src/renderer/components/MainMenu/index.js
@@ -1,8 +1,10 @@
 import PropTypes from "prop-types";
 import React from "react";
 
-import Config from "./Config";
-import Button from "./Button";
+import Config from "../Config";
+import Button from "../Button";
+
+import css from "./index.css";
 
 export default function MainMenu({
   config,
@@ -12,14 +14,14 @@ export default function MainMenu({
   exit
 }) {
   return (
-    <>
+    <div className={css.menu}>
       <h1>IHydra Launcher</h1>
       <Config config={config} />
       <Button onClick={tryInstall}>Install IHydra</Button>
       <Button onClick={goBackToWhich}>Find Jupyter</Button>
       <Button onClick={launchJupyter}>Launch Jupyter</Button>
       <Button onClick={exit}>Exit</Button>
-    </>
+    </div>
   );
 }
 
